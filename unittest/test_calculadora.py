6b5b9155db1f3c8b44ce79088e7426b91e8d98cc
juliopompeu95 <juliopompeu95@gmail.com>
2022-04-26 23:20:21 -0300

    teste

--- unittest/test_calculadora.py
+++ unittest/test_calculadora.py
@@ -1,10 +1,10 @@
 import unittest
-from teste import somas
+from soma import adicao
 
 
 class TestCalculadora(unittest.TestCase):
     def Test_5_e_5_retorna_10(self):
-        self.assertEqual(somas(5, 5), 10)
+        self.assertEqual(adicao(5, 5), 10)
 
 
 unittest.main(verbosity=2)
